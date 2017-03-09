06124e5b7667875f369443296f81a503df0f91b5
krega <kamil.regoc@gmail.com>
2017-03-09 22:05:18 +0100

    xml coniguration added

--- Copy of personal-finance-assistant-xml/src/main/java/ConsoleApplication.java
+++ Copy of personal-finance-assistant-xml/src/main/java/ConsoleApplication.java
@@ -2,17 +2,17 @@ import java.math.BigDecimal;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ConsoleApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext contex = new AnnotationConfigApplicationContext(AppConfig.class);
-		RateConversionService service = contex.getBean(RateConversionService.class);
+		ApplicationContext context = new ClassPathXmlApplicationContext("/appconfig.xml");
+		RateConversionService service = context.getBean(RateConversionService.class);
 		service.convertAmount(BigDecimal.valueOf(123.00), "EUR");
 		
 		
-		SecondRateConversionService service2 = contex.getBean(SecondRateConversionService.class);
-		service2.convertAmount(BigDecimal.valueOf(1244.00), "EUR");
+		
 	}
 	
 
